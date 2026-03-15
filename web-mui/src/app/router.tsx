cf2857788a85f9aed1548dc48f029154371854a4
Ira Stefan <ira.stefan91@gmail.com>
2026-03-15 13:59:30 +0200

    feat: add basename to router configuration for environment-specific routing

--- web-mui/src/app/router.tsx
+++ web-mui/src/app/router.tsx
@@ -97,4 +97,6 @@ export const router = createBrowserRouter([
       }
     ]
   }
-]);
+], {
+  basename: import.meta.env.BASE_URL
+});
--- web-mui/vite.config.ts
+++ web-mui/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? "/smart-food-plan/" : "/",
   plugins: [react()],
   resolve: {
     alias: {
